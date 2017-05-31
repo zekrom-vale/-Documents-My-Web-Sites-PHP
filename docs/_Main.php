972e38aa2a53eefc5b699671c14bd1d72d93e553
zekrom-vale <sgraven66@gmail.com>
2017-05-31 17:57:59 -0500

    Revert "Add files via upload"
    This reverts commit d16fef5cc207581b17b75d4d05f85b719d6cda00.

--- docs/_Main.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define("p", "<p>", FALSE);
-    define("bp", "</p>", FALSE);
-    define("Gb".[$i], $GLOBALS.[$i]);// can't do
-    define("br", "<br/>");
-    //<DOCTYPE html> is not required as just a php insrtion page
-?>
